9d2676ba5f39d2011414c5da97a6b0dbd45bd46e
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2023-01-07 13:32:46 +0100

    fix IfThenElse

--- src/main/java/fr/ensimag/deca/tree/IfThenElse.java
+++ src/main/java/fr/ensimag/deca/tree/IfThenElse.java
@@ -16,8 +16,8 @@ import org.apache.commons.lang.Validate;
  * @date 01/01/2023
  */
 public class IfThenElse extends AbstractInst {
-    
-    private final AbstractExpr condition; 
+
+    private final AbstractExpr condition;
     private final ListInst thenBranch;
     private ListInst elseBranch;
 
@@ -29,7 +29,7 @@ public class IfThenElse extends AbstractInst {
         this.thenBranch = thenBranch;
         this.elseBranch = elseBranch;
     }
-    
+
     @Override
     protected void verifyInst(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass, Type returnType)
@@ -43,12 +43,22 @@ public class IfThenElse extends AbstractInst {
 
     @Override
     public void decompile(IndentPrintStream s) {
-        throw new UnsupportedOperationException("not yet implemented");
+        s.print("if (");
+        condition.decompile(s);
+        s.println(") {");
+        s.indent();
+        thenBranch.decompile(s);
+        s.unindent();
+        s.println("}");
+        s.println("else {");
+        s.indent();
+        elseBranch.decompile(s);
+        s.unindent();
+        s.print("}");
     }
 
     @Override
-    protected
-    void iterChildren(TreeFunction f) {
+    protected void iterChildren(TreeFunction f) {
         condition.iter(f);
         thenBranch.iter(f);
         elseBranch.iter(f);

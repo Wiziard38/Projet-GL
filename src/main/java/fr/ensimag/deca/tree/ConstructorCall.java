0f86ddad760234f9df51fc4d859e1eef2957a25b
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2023-01-11 19:11:36 +0100

    fix

--- src/main/java/fr/ensimag/deca/tree/ConstructorCall.java
+++ src/main/java/fr/ensimag/deca/tree/ConstructorCall.java
@@ -1,9 +1,15 @@
 package fr.ensimag.deca.tree;
 
+import org.apache.commons.lang.Validate;
+
 public class ConstructorCall extends AbstractCall {
 
+    private ListExpr args = new ListExpr();
+
     public ConstructorCall(AbstractIdentifier name, ListExpr args) {
-        super(name, args);
+        super(name);
+        Validate.notNull(args);
+        this.args = args;
     }
 
 }

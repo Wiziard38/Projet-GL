bd5d8800155e6fdce7ea01f1a1db1a32a2c5c146
Puguix <paul.paulgauthier@outlook.fr>
2023-01-16 14:21:07 +0100

    base arm

--- src/main/java/fr/ensimag/deca/CompilerOptions.java
+++ src/main/java/fr/ensimag/deca/CompilerOptions.java
@@ -17,9 +17,10 @@ import org.apache.log4j.Logger;
  */
 public class CompilerOptions {
     public static final int QUIET = 0;
-    public static final int INFO  = 1;
+    public static final int INFO = 1;
     public static final int DEBUG = 2;
     public static final int TRACE = 3;
+
     public int getDebug() {
         return debug;
     }
@@ -36,7 +37,7 @@ public class CompilerOptions {
         return verification;
     }
 
-    public boolean geCompileInARM() {
+    public boolean getCompileInARM() {
         return compileInARM;
     }
 
@@ -51,7 +52,7 @@ public class CompilerOptions {
     public int getnumberRegisters() {
         return numberRegisters;
     }
-    
+
     public List<File> getSourceFiles() {
         return Collections.unmodifiableList(sourceFiles);
     }
@@ -66,21 +67,20 @@ public class CompilerOptions {
     private int numberRegisters = 16;
     private List<File> sourceFiles = new ArrayList<File>();
 
-    
     public void parseArgs(String[] args) throws CLIException {
-        
+
         int index = 0;
         while (index < args.length) {
 
             String arg = args[index];
             switch (arg) {
-                
+
                 case "-b": // banner
                     this.printBanner = true;
                     if (args.length != 1) {
                         throw new CLIException("Option '-b' must be used alone");
                     }
-                
+
                 case "-p": // parse
                     this.parsing = true;
                     if (this.verification || this.compileInARM) {
@@ -98,14 +98,14 @@ public class CompilerOptions {
                 case "-n": // no check
                     this.check = true;
                     break;
-                
+
                 case "-arm": // ARM
                     this.compileInARM = true;
                     if (this.parsing || this.verification) {
                         throw new CLIException("Cannot use options '-p' and '-v' and '-arm' together");
                     }
                     break;
- 
+
                 case "-d": // debug
                     this.debug += 1;
                     break;
@@ -113,14 +113,13 @@ public class CompilerOptions {
                 case "-P": // parallele
                     this.parallel = true;
                     break;
-                
+
                 case "-r": // registers
                     index++;
                     int number;
-                    try{
+                    try {
                         number = Integer.parseInt(args[index]);
-                    }
-                    catch (NumberFormatException ex){
+                    } catch (NumberFormatException ex) {
                         throw new CLIException("Must enter a number after '-r' option");
                     }
                     if (number < 4 || number > 16) {
@@ -128,7 +127,7 @@ public class CompilerOptions {
                     }
                     numberRegisters = number;
                     break;
-                    
+
                 case "-w": // pas pour l'instant
 
                 default:
@@ -153,15 +152,20 @@ public class CompilerOptions {
         Logger logger = Logger.getRootLogger();
         // map command-line debug option to log4j's level.
         switch (getDebug()) {
-        case QUIET: break; // keep default
-        case INFO:
-            logger.setLevel(Level.INFO); break;
-        case DEBUG:
-            logger.setLevel(Level.DEBUG); break;
-        case TRACE:
-            logger.setLevel(Level.TRACE); break;
-        default:
-            logger.setLevel(Level.ALL); break;
+            case QUIET:
+                break; // keep default
+            case INFO:
+                logger.setLevel(Level.INFO);
+                break;
+            case DEBUG:
+                logger.setLevel(Level.DEBUG);
+                break;
+            case TRACE:
+                logger.setLevel(Level.TRACE);
+                break;
+            default:
+                logger.setLevel(Level.ALL);
+                break;
         }
         logger.info("Application-wide trace level set to " + logger.getLevel());
 
@@ -177,34 +181,34 @@ public class CompilerOptions {
 
     protected void displayUsage() {
         String usage = String.join(
-            System.getProperty("line.separator"),
-            "usage: decac [[-p | -v | -arm] [-n] [-r X] [-d]* [-P] <fichier deca>...]",
-            "             | [-b]");
-        //TODO add -w option ?
+                System.getProperty("line.separator"),
+                "usage: decac [[-p | -v | -arm] [-n] [-r X] [-d]* [-P] <fichier deca>...]",
+                "             | [-b]");
+        // TODO add -w option ?
         System.out.println(usage);
 
         String options = String.join(
-            System.getProperty("line.separator"),
-            System.getProperty("line.separator"),
-            "  -b (banner)      : affiche une bannière indiquant le nom de l'équipe", 
-            "  -p (parse)       : arrête decac après l'étape de construction de l'arbre,",
-            "                     et affiche la décompilation de ce dernier (i.e. s'il n'y a", 
-            "                     qu'un fichier source à compiler, la sortie doit être un",
-            "                     programme deca syntaxiquement correct)",
-            " -v (verification) : arrête decac après l'étape de vérifications (ne produit", 
-            "                     aucune sortie en l'absence d'erreur)",
-            " -n (no check)     : supprime les tests à l'exécution spécifiés dans les points", 
-            "                     11.1 et 11.3 de la sémantique de Deca.",
-            " -r X (registers)  : limite les registres banalisés disponibles à R0 ... R{X-1},",
-            "                     avec 4 <= X <= 16",
-            " -d (debug)        : active les traces de debug. Répéter l'option plusieurs fois", 
-            "                     pour avoir plus de traces.",
-            " -P (parallel)     : s'il y a plusieurs fichiers sources, lance la compilation",
-            "                     des fichiers en parallèle (pour accélérer la compilation)",
-            " -arm (ARM)        : génère du code assembleur qui sera executable sur un", 
-            "                     processeur ARM-v6",
-            System.getProperty("line.separator"));
+                System.getProperty("line.separator"),
+                System.getProperty("line.separator"),
+                "  -b (banner)      : affiche une bannière indiquant le nom de l'équipe",
+                "  -p (parse)       : arrête decac après l'étape de construction de l'arbre,",
+                "                     et affiche la décompilation de ce dernier (i.e. s'il n'y a",
+                "                     qu'un fichier source à compiler, la sortie doit être un",
+                "                     programme deca syntaxiquement correct)",
+                " -v (verification) : arrête decac après l'étape de vérifications (ne produit",
+                "                     aucune sortie en l'absence d'erreur)",
+                " -n (no check)     : supprime les tests à l'exécution spécifiés dans les points",
+                "                     11.1 et 11.3 de la sémantique de Deca.",
+                " -r X (registers)  : limite les registres banalisés disponibles à R0 ... R{X-1},",
+                "                     avec 4 <= X <= 16",
+                " -d (debug)        : active les traces de debug. Répéter l'option plusieurs fois",
+                "                     pour avoir plus de traces.",
+                " -P (parallel)     : s'il y a plusieurs fichiers sources, lance la compilation",
+                "                     des fichiers en parallèle (pour accélérer la compilation)",
+                " -arm (ARM)        : génère du code assembleur qui sera executable sur un",
+                "                     processeur ARM-v6",
+                System.getProperty("line.separator"));
 
-        System.out.println(options);   
+        System.out.println(options);
     }
 }

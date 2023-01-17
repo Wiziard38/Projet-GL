5d14c84001ef0d7189091d06b573089f3ca2bc9e
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2023-01-17 11:37:10 +0100

    ajout scripts arm

---
+++ src/test/script/test_arm_complet.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# Script pour tester tous les test de codegen mais avec arm
+
+# On lance decac sur les tests et on vérifie qu'ils sont bien valides ou invalides
+
+
+test_arm_invalid.sh
+test_arm_valid.sh
